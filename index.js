39a85fc31d2d0de3d1ccca6d7ea74b6c28695a02
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-07 06:57:48 +0000

    this file updated

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
-let balance = 500.00;
+let balance = 500.0;
+
+class Deposit {
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // Update the balance in the account
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
 
 class Withdrawal {
-
-  constructor(amount) {
+  // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+
+  // Update the balance in the account
+  commit() {
+    this.account.balance -= this.amount;
+  }
+}
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    // Calculate the balance using the transaction objects.
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
   }
-
 }
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
-
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+
+console.log('Ending Balance:', myAccount.balance);
+
+t3 = new Deposit(120.0);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+const myAccount = new Account('snow-patrol');
 
 console.log('Balance:', balance);
